2ce3acd23d10e8aa0529dcb96ddc781d5fb0d7b5
marekgorskiwsb@gmail.com <gorskimarekgm@gmail.com>
2021-10-27 16:09:06 +0200

    from stev thomas will get just first letters 'stw'

--- script.js
+++ script.js
@@ -89,5 +89,8 @@ const displayMovements = function(movements){
 displayMovements(account1.movements);
 
 const user = 'Steven Thomas Williams'; //i need stw abbrevation
-const username = user.toLowerCase().split(' ');
+const username = user.toLowerCase().split(' ').map(function(name){
+  return name[0];
+}).join(''); //join letters without any seperator
+
 console.log(username)
