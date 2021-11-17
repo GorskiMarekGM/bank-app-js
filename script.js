439967a2f2690fc3009eb9155ee78a69cb195bd2
marekgorskiwsb@gmail.com <gorskimarekgm@gmail.com>
2021-11-17 16:24:24 +0100

    request loan button

--- script.js
+++ script.js
@@ -236,6 +236,16 @@ btnClose.addEventListener('click',function(e){
     inputClosePin.value = inputCloseUsername.value = ''
 })
 
+btnLoan.addEventListener('click',function(e){
+  e.preventDefault()
+  const amount = Number(inputLoanAmount.value)
+  if(amount > 0 &&
+    currentAcount.movements.some( mov => mov >= amount * 0.1)){
+      currentAcount.movements.push(amount)
+      updateUI(currentAcount)
+  }
+})
+
 // todo - HOMEWORK
 // timer at the end
 // logging out
